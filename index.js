e265b1b447b8894c01d5e7817ba853bac5807fed
Chinmay Konchada <chinmaykonchada@gmail.com>
2024-07-13 14:58:50 +0530

    rough draft of news interface and data form api is shown

---
+++ index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import axios from "axios";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+const API_URL="https://newsapi.org/v2/top-headlines";
+const API_KEY="7327c9005d9d4cad817984caf24e7260";
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', (req, res) => {
+    res.render("index.ejs");
+})
+app.post('/headlines', async (req, res) => {
+    try{
+        const response = await axios.get(API_URL + `?country=${req.body.country}`+`&apiKey=${API_KEY}`);
+        const result =response.data;
+        // console.log(result.totalResults);
+        res.render("index.ejs", {news: result})
+    }catch (error){
+        console.error("Failed to make request:", error.message);
+    }
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
